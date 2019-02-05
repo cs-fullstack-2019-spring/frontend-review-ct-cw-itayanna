d89c3dfcc9fa374ec8da05cdddf9ad4875bc9d9c
itayanna <icage97@gmail.com>
2019-02-05 16:07:44 -0600

    classwork attempted

---
+++ j.js
@@ -0,0 +1,70 @@
+
+
+var gameSpaces= document.querySelectorAll('td');
+var currentPlayer= 1;
+function clickedSquare(e){
+    console.log("clicked");
+    if (currentPlayer===1 && e.target.innerHTML === "" ){
+        e.target.innerHTML = "X";
+        currentPlayer=2
+    }
+    else if (e.target.innerHTML === "" ) {
+        e.target.innerHTML= "O";
+        currentPlayer=1
+    }
+    checkForwinner();
+}
+
+for (var i = 0; i < gameSpaces.length; i++) {
+    gameSpaces[i].addEventListener('click', clickedSquare);
+}
+
+
+
+function checkForwinner() {
+
+console.log(1);
+if((gameSpaces[0].innerText=== gameSpaces[1].innerText &&gameSpaces[1].innerText === gameSpaces[2].innerText && gameSpaces[2].innerText==="X")
+    ||(gameSpaces[3].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[5].innerText && gameSpaces[5].innerText==="X")
+    ||(gameSpaces[6].innerText=== gameSpaces[7].innerText &&gameSpaces[7].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+    ||(gameSpaces[0].innerText=== gameSpaces[3].innerText &&gameSpaces[3].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="X")
+    ||(gameSpaces[1].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[7].innerText && gameSpaces[7].innerText==="X")
+    ||(gameSpaces[2].innerText=== gameSpaces[5].innerText &&gameSpaces[5].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+    ||(gameSpaces[0].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+    ||(gameSpaces[2].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="X"))
+{
+    alert("X wins");
+    return false;
+}
+else if((gameSpaces[0].innerText=== gameSpaces[1].innerText &&gameSpaces[1].innerText === gameSpaces[2].innerText && gameSpaces[2].innerText==="O")
+    ||(gameSpaces[3].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[5].innerText && gameSpaces[5].innerText==="O")
+    ||(gameSpaces[6].innerText=== gameSpaces[7].innerText &&gameSpaces[7].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+    ||(gameSpaces[0].innerText=== gameSpaces[3].innerText &&gameSpaces[3].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="O")
+    ||(gameSpaces[1].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[7].innerText && gameSpaces[7].innerText==="O")
+    ||(gameSpaces[2].innerText=== gameSpaces[5].innerText &&gameSpaces[5].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+    ||(gameSpaces[0].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+    ||(gameSpaces[2].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="O")) {
+    alert("O wins");
+    return false;
+}
+
+
+}
+
+
+var btn= document.querySelector("button");
+
+btn.onclick= function() {
+    console.log(3);
+    for (var i= 0; i<gameSpaces.length; i++){
+        gameSpaces[i].innerHTML= ''
+    }
+};
+
+
+
+
+
+
+
+
